51f4664c2854725308628ff76dc0a93503c3e278
coderRahulPancholi <rahulpancholi3937@gmail.com>
2026-02-14 14:25:31 +0530

    feat: Add Role and Custom DocPerm fixtures.

--- safarwaala/hooks.py
+++ safarwaala/hooks.py
@@ -9,6 +9,8 @@ app_license = "mit"
 # ------------------
 
 # required_apps = []
+fixtures = ["Role", "Custom DocPerm"]
+
 
 # Each item in the list will be shown as an app in the apps page
 # add_to_apps_screen = [
